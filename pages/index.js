2f6314ce18ce2b4c8e2e2615af0e9333ab44ac98
Alan <alan_toro@hotmail.com>
2021-02-01 15:00:59 -0300

    refactor functional component

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,33 @@
 import React from 'react';
 
-export default class Home extends React.Component {
-    constructor() {
-        console.log('Home.constructor()');
-        super();
-    }
-
-    render() {
-        console.log('home.rendersape()');
-
-        return (
-          <div className="iframeContainer">
-            <iframe src="https://mercadolibre.com.ar" title="iframe mercadolibre" height="100%" wifth="100%" />
-            <style jsx>
-              {`
-                .home {
-                    background-color: black;
-                }
-                .iframeContainer {
-                    width: 100vw;
-                    height: 100vh;
-                    overflow: hidden;
-                }
-                iframe {
-                    outline: none;
-                    margin-top: -60px;
-                    width: 100%;
-                    border: none;
-                    pointer-events: none;
-                }
-            `}
-            </style>
-          </div>
-
-        );
-    }
+export default function Home() {
+  return (
+    <div className="iframeContainer">
+      <iframe
+        src="https://mercadolibre.com.ar"
+        title="iframe mercadolibre"
+        height="100%"
+        wifth="100%"
+      />
+      <style jsx>
+        {`
+          .home {
+            background-color: black;
+          }
+          .iframeContainer {
+            width: 100vw;
+            height: 100vh;
+            overflow: hidden;
+          }
+          iframe {
+            outline: none;
+            margin-top: -60px;
+            width: 100%;
+            border: none;
+            pointer-events: none;
+          }
+        `}
+      </style>
+    </div>
+  );
 }
