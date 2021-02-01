b9abd6c7c76eefd4252dca25d053cfa4245f3fe8
Alan <alan_toro@hotmail.com>
2021-02-01 15:01:44 -0300

    dynamic categories from api

--- components/breadcrumb/index.js
+++ components/breadcrumb/index.js
@@ -1,24 +1,37 @@
 import React from 'react';
-const categorias = ['categoria 1', 'categoria 2', 'categoria 3']
-const renderCrumbs = (props) => categorias.map((i) => (<li key={props.id} className="breadcrumb-item"><a href="/" className="color-2">{i}</a></li>));
-const BreadCrumb = (props) => (
-  <nav aria-label="breadcrumb">
-    <ol className="breadcrumb">
-      {renderCrumbs(props)}
-    </ol>
-    <style jsx>
-      {`
-        .breadcrumb {
+import PropTypes from 'prop-types';
+
+const renderCrumbs = (categories) => categories.map((i) => (
+  <li key={i.id} className="breadcrumb-item">
+    <a href={`/items?search=${i.name}`} className="color-2">
+      {i.name}
+    </a>
+  </li>
+));
+export default function BreadCrumb(props) {
+  const { categories } = props;
+  return (
+    <nav aria-label="breadcrumb">
+      <ol className="breadcrumb">{renderCrumbs(categories)}</ol>
+      <style jsx>
+        {`
+          .breadcrumb {
             background-color: transparent;
             margin-bottom: 0;
             padding: 16px 0;
-        }
-        p {
-            margin-bottom:0
-        }
-      `}
-    </style>
-  </nav>
-
-);
-export default BreadCrumb;
+          }
+          p {
+            margin-bottom: 0;
+          }
+        `}
+      </style>
+    </nav>
+  );
+}
+BreadCrumb.propTypes = {
+  categories: PropTypes.arrayOf(PropTypes.object),
+};
+// Same approach for defaultProps too
+BreadCrumb.defaultProps = {
+  categories: [],
+};
